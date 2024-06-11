8dafec5b3f10e44b7f12c3db30c8d9d82fe3b775
Dee Lee <hoondanglee@gmail.com>
2024-06-11 08:52:10 -0700

    comments out

--- server/googleOauth.js
+++ server/googleOauth.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class googleOauth {
 }
-googleOauth.id = '322958672607-k31b97t9teq92hbd58h45rpsk6an1e4c.apps.googleusercontent.com';
-googleOauth.secret = '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';
+//googleOauth.id = '322958672607-k31b97t9teq92hbd58h45rpsk6an1e4c.apps.googleusercontent.com';
+//googleOauth.secret = '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';
 exports.default = googleOauth;
 //# sourceMappingURL=googleOauth.js.map
