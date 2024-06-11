98aea73216387bc1ef7f76f4d3a86285d1552d81
Dee Lee <hoondanglee@gmail.com>
2024-06-11 09:00:41 -0700

    removed google secret

--- server/googleOauth.js
+++ server/googleOauth.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class googleOauth {
 }
-//googleOauth.id = '322958672607-k31b97t9teq92hbd58h45rpsk6an1e4c.apps.googleusercontent.com';
-//googleOauth.secret = '';
+googleOauth.id = '322958672607-k31b97t9teq92hbd58h45rpsk6an1e4c.apps.googleusercontent.com';
+googleOauth.secret = '';
 exports.default = googleOauth;
 //# sourceMappingURL=googleOauth.js.map
